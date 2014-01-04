0edb22d34e8c91f7247d7cb16043163c3970dbe2
Anish Agarwal <anish.agarwal112@gmail.com>
2014-01-04 22:35:07 +0530

    to do list added

---
+++ style.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function(){
+    $("#button").click(function(){
+        var toAdd=  $('input[name=checkListItem]').val();
+    $(".list").append('<div class="item">' + toAdd + '</div>');
+    }); 
+    $(document).on('click', '.item', function() {
+    $(this).remove();
+    });
+
+});
